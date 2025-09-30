6b2e8179e364e40b5171dfe59874e6eac76e4f10
nohseon-song <snspe@naver.com>
2025-09-30 11:05:37 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = "wrapper-v1";
+const APP_SHELL = ["./","./index.html","./manifest.json","./icons/icon-192.png","./icons/icon-256.png","./icons/icon-512.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(APP_SHELL)));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
